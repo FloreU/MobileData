1e0a6644ce7a7f07b00825172cb0422c3f4897bb
望舒凌霄 <wangshulingxiao@163.com>
2017-05-05 16:43:05 +0800

    均值+9

--- statistics/summary.py
+++ statistics/summary.py
@@ -67,12 +67,70 @@ def save_table(out_table, result_obj, id_field):
     return out_table
 
 
-def create_summary_model_table(out_path, template_table, id_field, field_list):
+def create_summary_model_table1(out_path, template_table, id_field, field_list):
     arcpy.CreateTable_management(out_path, template_table, "", "")
     out_template = out_path + "/" + template_table
     arcpy.AddField_management(out_template, id_field, "LONG")
     for field in field_list:
-        arcpy.AddField_management(out_template, ("Sum_"+field), "DOUBLE")
+        arcpy.AddField_management(out_template, ("Sum_" + field), "DOUBLE")
         arcpy.AddField_management(out_template, ("Count_" + field), "DOUBLE")
         arcpy.AddField_management(out_template, ("Average_" + field), "DOUBLE")
     return out_template
+
+
+def create_summary_model_table2(out_path, template_table, id_field, field_list):
+    arcpy.CreateTable_management(out_path, template_table, "", "")
+    out_template = out_path + "/" + template_table
+    arcpy.AddField_management(out_template, id_field, "LONG")
+    for field in field_list:
+        arcpy.AddField_management(out_template, ("Average_" + field), "DOUBLE")
+    return out_template
+
+
+def field_calculate2(in_feature, id_field, field_list, time_point, expression, result_obj={}):
+    feature_field_list = sys_arcpy.get_all_fields(in_feature)
+    real_field_list = []
+    for field in field_list:
+        if field in feature_field_list:
+            real_field_list.append(field)
+    field_list = real_field_list
+    cursor = arcpy.SearchCursor(in_feature, expression)
+    for row in cursor:
+        region_id = row.getValue(id_field)
+        if region_id not in result_obj:
+            tmp_obj = {time_point: {}}
+            for field in field_list:
+                tmp_obj[time_point][field] = {"Sum": 0, "Count": 0}
+            result_obj[region_id] = tmp_obj
+        region_obj = result_obj[region_id]
+        for field in field_list:
+            field_value = row.getValue(field)
+            region_obj[time_point][field]["Sum"] += field_value
+            region_obj[time_point][field]["Count"] += 1
+        result_obj[region_id] = region_obj
+    del cursor, row
+    return result_obj
+
+
+def calculate_average2(result_obj, num):
+    for region_id in result_obj:
+        for time_point in result_obj[region_id]:
+            for field in result_obj[region_id][time_point]:
+                result_obj[region_id][time_point][field]["RealAverage"] = (
+                    result_obj[region_id][time_point][field]["Sum"] / num
+                )
+    return result_obj
+
+
+def save_table2(out_table, result_obj, id_field, time_point):
+    cursor = arcpy.InsertCursor(out_table)
+    for region_id in result_obj:
+        row = cursor.newRow()
+        row.setValue(id_field, region_id)
+        tmp_obj = result_obj[region_id][time_point]
+        for field in tmp_obj:
+            val_aver = tmp_obj[field]["RealAverage"]
+            row.setValue(("Average_" + field), val_aver)
+        cursor.insertRow(row)
+    # del cursor, row
+    return out_table
--- statistics/summaryAgeSexTime.py
+++ statistics/summaryAgeSexTime.py
@@ -3,7 +3,6 @@ import arcpy
 import sys
 
 from statistics import summary
-from statistics import time_list
 from statistics import var_access
 
 reload(sys)
@@ -13,7 +12,13 @@ sys.setdefaultencoding('utf-8')
 tmp_var_dir = "E:/InformationCenter/MobileData/tmp_var"
 date_filed = "TIME_DUR"
 region_id_field = "GRID_ID"
-time_interval = ["09:00:00", "17:00:00"]
+time_points = ["09:00:00", "17:00:00"]
+year_month = "2016-06-"
+work_day = [1, 2, 3,
+            6, 7, 8, 9, 10,
+            13, 14, 15, 16, 17,
+            20, 21, 22, 23, 24,
+            27, 28, 29, 30]
 template_table = "SUM_TEMPLATE"
 curr_type = "Sex"
 # 年龄——时刻表
@@ -49,26 +54,29 @@ try:
     table_name_list = var_access.load_var(c_var_file)
     out_template = summary.create_summary_model_table1(c_sum_gdb, template_table, region_id_field, c_field_list)
     print("模板创建 -- 100%")
-    t_num = time_list.time_difference(time_interval[0], time_interval[1])
+    day_num = len(work_day)
     sum_table_list = []
+    work_day_result = {}
     for table in table_name_list:
         name_array = table.split("_")
         table_date = name_array[-3] + "-" + name_array[-2] + "-" + name_array[-1]
-        start_time = table_date + " " + time_interval[0]
-        end_time = table_date + " " + time_interval[1]
-        expression = date_filed + " >= '" + start_time + "' AND " + date_filed + " <= '" + end_time + "'"
-        one_day_result = summary.field_calculate(c_gdb + "／" + table, region_id_field, c_field_list, expression)
-        one_day_result = summary.calculate_average(one_day_result, t_num)
-        sum_one_day_table = (table + "_SUM_" +
-                             time_interval[0].replace(":", "") + "_" +
-                             time_interval[1].replace(":", ""))
-        print(table)
-        arcpy.CreateTable_management(c_sum_gdb, sum_one_day_table, out_template, "")
-        summary.save_table(sum_one_day_table, one_day_result, region_id_field)
-        print(sum_one_day_table)
-        sum_table_list.append(sum_one_day_table)
-    save_var_name = c_var_file[0:-4]
-    var_access.save_var(sum_table_list, (tmp_var_dir + "/" + save_var_name + ".pkl"))
-    var_access.save_json(sum_table_list, (tmp_var_dir + "/" + save_var_name + ".txt"))
+        if int(name_array[-1]) not in work_day:
+            continue
+        for time_point in time_points:
+            this_time = table_date + " " + time_point
+            expression = date_filed + " = '" + this_time + "'"
+            work_day_result = summary.field_calculate2(
+                c_gdb + "／" + table, region_id_field,
+                c_field_list, time_point, expression, work_day_result
+            )
+            print(table+"_"+time_point)
+    work_day_result = summary.calculate_average2(work_day_result, day_num)
+
+    for time_point in time_points:
+        sum_work_day_time_point_table = "SUM_" + curr_type + "_" + time_point.replace(":", "_")
+        arcpy.CreateTable_management(c_sum_gdb, sum_work_day_time_point_table, out_template, "")
+        summary.save_table2(sum_work_day_time_point_table, work_day_result, region_id_field, time_point)
+        sum_table_list.append(sum_work_day_time_point_table)
+        print(sum_work_day_time_point_table)
 except Exception as err:
     print(err.args[0])
